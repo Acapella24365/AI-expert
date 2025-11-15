1d6e51997cce59e5b3a6dfcaae6e129b5b237e29
Acapella24365 <syedahayaamzahra@gmail.com>
2025-11-15 17:16:58 +0500

    1st ACP

---
+++ Hello AI (acp) .py
@@ -0,0 +1,16 @@
+print("Hello! My name is AI Bot! What's yours?")
+
+name = input()
+print(f"Nice to meet you {name}")
+
+print("What's your favourite food?")
+
+food = input().lower
+
+if food == "steak":
+    print(f"Same {name}!")
+else:
+    print(f"Good food choice !!")
+
+
+print(f"It was nice chatting with you {name}, goodbye!")
