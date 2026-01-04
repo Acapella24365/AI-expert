a1b180cf934774c9af529cc3e82953f7b2ecf08c
Acapella24365 <syedahayaamzahra@gmail.com>
2026-01-04 14:08:44 +0500

    image annotation acp

--- Image Annotation/Image Annotation acp.py
+++ Image Annotation/Image Annotation acp.py
@@ -1,7 +1,7 @@
 import cv2
 import matplotlib.pyplot as plt
 
-image_path = 'example.jpg'
+image_path = 'image_acp.jpg'
 image = cv2.imread(image_path)
 
 image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
